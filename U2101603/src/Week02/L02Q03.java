cd24904bcb42458cd994d4835d8b479963df1760
Khairul <>
2022-10-28 16:53:43 +0800

    Add Question 3

---
+++ U2101603/src/Week02/L02Q03.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week02;
+
+/**
+ *
+ * @author User
+ */
+public class L02Q03 {
+    public static void main(String[] args) {
+        
+    }
+}
