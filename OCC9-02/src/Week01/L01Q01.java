929c3dd78c25488161ec73a232e13e8f3e170513
Azhar Aizad <Azhar Aizad@DESKTOP-T013B16>
2022-10-28 23:00:35 +0800

    Q1 its my profile try to add you guys profile just continue the language

--- OCC9-02/src/Week01/L01Q01.java
+++ OCC9-02/src/Week01/L01Q01.java
@@ -9,5 +9,11 @@ package Week01;
  * @author Azhar Aizad
  */
 public class L01Q01 {
+    public static void main(String[] args) {
+        System.out.println("name: azhar");
+        System.out.println("matrix: U2100687");
+        System.out.println("Address: Johor Bahru");
+        System.out.println("Email: U2100687@siswa.um.edu.my");
+    }
    
 }
