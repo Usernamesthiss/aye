6e200cf98e6ef88be707aef6ddd0065aeba49629
Azhar Aizad <Azhar Aizad@DESKTOP-T013B16>
2022-10-28 11:17:04 +0800

    tukar nama

--- OCC9-02/src/Week01/L01Q01.java
+++ OCC9-02/src/Week01/L01Q01.java
@@ -1,13 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.L01Q01 to edit this template
  */
-package Week02;
+package Week01;
 
 /**
  *
  * @author Azhar Aizad
  */
-public class NewClass {
+public class L01Q01 {
     
 }
