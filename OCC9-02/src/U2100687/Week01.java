18d60b743761e33f576c8590622eb8180995f473
Azhar Aizad <Azhar Aizad@DESKTOP-T013B16>
2022-11-03 00:08:15 +0800

    New folder

---
+++ OCC9-02/src/U2100687/Week01.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package U2100687;
+
+/**
+ *
+ * @author Azhar Aizad
+ */
+public class Week01 {
+    
+}
