aea6de83c6d95f0238275a72ac59ce29576fed30
Azhar Aizad <Azhar Aizad@DESKTOP-T013B16>
2022-11-03 00:04:14 +0800

    New folder

--- OCC9-02/src/Test01/L01Q01.java
+++ OCC9-02/src/Test01/L01Q01.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.L01Q01 to edit this template
  */
-package Week01;
+package Test01;
 
 /**
  *
