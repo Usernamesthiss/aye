80d8e70b059f4748062dec290cbb405abc542944
Azhar Aizad <Azhar Aizad@DESKTOP-T013B16>
2022-11-03 21:05:45 +0800

    Merge origin/master

--- src/Week01/L01Q01.java
+++ src/Week01/L01Q01.java
@@ -9,5 +9,12 @@ package Week01;
  * @author Azhar Aizad
  */
 public class L01Q01 {
+    public static void main(String[] args) {
+        System.out.println("Name: Azhar Aizad");
+        System.out.println("Matric No: U2100687");
+        System.out.println("Address: Johor Bahru");
+        System.out.println("Email: U2100687@siswa.um.edu.my");
+        System.out.println("Contact: 011 1778 4797");
+    }
     
 }
