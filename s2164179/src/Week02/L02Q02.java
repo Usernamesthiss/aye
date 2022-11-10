a2332bedcd73ddeff5a2a7665acf89194f3d86d7
Irfanur <Irfanur@DESKTOP-JAHRIGO>
2022-11-11 00:51:34 +0800

    Merge origin/master

---
+++ s2164179/src/Week02/L02Q02.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week02;
+
+/**
+ *
+ * @author Irfanur
+ */
+public class L02Q02 {
+    public static void main(String[] args) {
+        
+    }
+}
