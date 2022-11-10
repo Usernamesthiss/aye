c4fc4329e6fbc04c09d373c6959f4a3f44df575a
Irfanur <Irfanur@DESKTOP-JAHRIGO>
2022-11-10 23:16:17 +0800

    Merge origin/master

---
+++ s2164179/src/Week01/L01Q03.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week01;
+
+/**
+ *
+ * @author Irfanur
+ */
+public class L01Q03 {
+    
+    public static void main(String[] args) {
+       System.out.println("   |                      ***       ");
+       System.out.println("   |                      ***       ");
+       System.out.println("3000                      ***       ");
+       System.out.println("   |                      ***       ");
+       System.out.println("   |                      ***       ");
+       System.out.println("   |                 ***  ***       ");
+       System.out.println("   |                 ***  ***       ");
+       System.out.println("2500  ***            ***  ***       ");
+       System.out.println("   |  ***            ***  ***       ");
+       System.out.println("   |  ***            ***  ***       ");
+       System.out.println("   |  ***            ***  ***       ");
+       System.out.println("   |  ***            ***  ***       ");
+       System.out.println("2000  ***       ***  ***  ***       ");
+       System.out.println("   |  ***       ***  ***  ***       ");
+       System.out.println("   |  ***       ***  ***  ***       ");
+       System.out.println("   |  ***       ***  ***  ***       ");
+       System.out.println("   |  ***  ***  ***  ***  ***       ");
+       System.out.println("1500  ***  ***  ***  ***  ***       ");
+       System.out.println("   |  ***  ***  ***  ***  ***       ");
+       System.out.println("   |  ***  ***  ***  ***  ***       ");
+       System.out.println("   |  ***  ***  ***  ***  ***       ");
+       System.out.println("   |  ***  ***  ***  ***  ***       ");
+       System.out.println("1000  ***  ***  ***  ***  ***       ");
+       System.out.println("   |  ***  ***  ***  ***  ***       ");
+       System.out.println("   |  ***  ***  ***  ***  ***  ***  ");
+       System.out.println("   |  ***  ***  ***  ***  ***  ***  ");
+       System.out.println("   |  ***  ***  ***  ***  ***  ***  ");
+       System.out.println("500   ***  ***  ***  ***  ***  ***  ");
+       System.out.println("   |  ***  ***  ***  ***  ***  ***  ");
+       System.out.println("   |  ***  ***  ***  ***  ***  ***  ");
+       System.out.println("   |  ***  ***  ***  ***  ***  ***  ");
+       System.out.println("   |  ***  ***  ***  ***  ***  ***  ");
+       System.out.println("   |__JAN__FEB__MAR__APR__MAY__JUN__>");
+       System.out.println("             (Years of 2016)         ");
+    
+    
+    }
+    
+   
+    
+}
