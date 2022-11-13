8d1aa7dd155ccaa451e29e8e0d8f9f7072939659
aviat <aviat@Flappyboi>
2022-11-13 15:24:23 +0800

    L!Q$

---
+++ L1Q4/src/main/java/com/mycompany/l1q4/L1Q4.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.l1q4;
+
+/**
+ *
+ * @author aviat
+ */
+public class L1Q4 {
+
+    public static void main(String[] args) {
+        int j=1,f=1,m=1,a=1,may=1,jun=1, sum=0;
+        System.out.print("January | ");
+
+while (j<=25) {
+    System.out.print("O");
+   sum+=j;
+   j++;
+}
+System.out.print("\n");
+sum=0;
+System.out.print("February| ");
+
+while (f<=16) {
+    System.out.print("O");
+   sum+=f;
+   f++;
+}
+System.out.print("\n");
+sum=0;
+System.out.print("March   | ");
+
+while (m<=20) {
+    System.out.print("O");
+   sum+=m;
+   m++;
+}
+System.out.print("\n");
+sum=0;
+System.out.print("April   | ");
+
+while (a<=27) {
+    System.out.print("O");
+   sum+=a;
+   a++;
+}
+System.out.print("\n");
+sum=0;
+System.out.print("May     | ");
+
+while (may<=32) {
+    System.out.print("O");
+   sum+=may;
+   may++;
+}
+System.out.print("\n");
+sum=0;
+System.out.print("June    | ");
+while (jun<=8) {
+    System.out.print("O");
+   sum+=jun;
+   jun++;
+}
+
+
+}
+
+
+        
+    }
+
